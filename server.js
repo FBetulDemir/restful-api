40148032732d1977c953205c62101a0dad2fb0fe
FBetulDemir <fatmabetuld@gmail.com>
2024-10-16 11:52:56 +0200

    Added get and post routers for users

--- server.js
+++ server.js
@@ -9,3 +9,37 @@ const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`Server is running in http://localhost:${PORT}`);
 });
+
+let users = [
+    {
+        id: 1,
+        name: 'Lisa',
+        surname: 'Erikson',
+        age: 37,
+    },
+    {
+        id: 2,
+        name: 'Joe',
+        surname: 'Jackson',
+        age: 45,
+    },
+];
+
+app.get('/users', (req, res) => {
+    res.json(users);
+});
+
+app.post('/users', (req, res) => {
+    const newUser = {
+        id: users.length + 1,
+        name: req.body.name,
+        surname: req.body.surname,
+        age: req.body.age,
+    };
+    users.push(newUser);
+    res.json({ message: 'User details added successfully!', user: newUser });
+});
+
+app.put('/users:id', (req, res) => {
+    const userId = parseInt(req.param.id);
+});

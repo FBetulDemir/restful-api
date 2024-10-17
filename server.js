65dc6cf0b8469909024369e6adf49cce3d60325b
FBetulDemir <fatmabetuld@gmail.com>
2024-10-17 08:33:02 +0200

    Added update user and delete user functions

--- server.js
+++ server.js
@@ -40,6 +40,37 @@ app.post('/users', (req, res) => {
     res.json({ message: 'User details added successfully!', user: newUser });
 });
 
-app.put('/users:id', (req, res) => {
-    const userId = parseInt(req.param.id);
+// app.put('/users/:id', (req, res) => {
+//     const userId = parseInt(req.param.id);
+//     const user = users.find((u) => u.id === userId);
+//     if (!user) {
+//         return res.status(404).json({ message: 'User not found!' });
+//     }
+//     user.name = req.body.name || user.name;
+//     user.surname = req.body.surname || user.surname;
+//     res.json({ message: 'User updated successfully!', user });
+// });
+
+app.put('/users/:id', (req, res) => {
+    const userId = parseInt(req.params.id);
+    const user = users.find((u) => u.id === userId);
+    if (!user) {
+        return res.status(404).json({ message: 'User not found!' });
+    }
+
+    const updatedUser = { ...user };
+
+    updatedUser.name = req.body.name || updatedUser.name;
+    updatedUser.surname = req.body.surname || updatedUser.surname;
+
+    const userIndex = users.findIndex((u) => u.id === userId);
+    users[userIndex] = updatedUser;
+
+    res.json({ message: 'User updated successfully!', user: updatedUser });
+});
+
+app.delete('/users/:id', (req, res) => {
+    const userId = parseInt(req.params.id);
+    users = users.filter((u) => u.id !== userId);
+    res.json({ message: 'User deleted succesfully!' });
 });
